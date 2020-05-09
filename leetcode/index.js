a749dea72218a246c25c2806b305acf7bee9fb4d
hzxfjmc@163.com <huangzuxing@youxin.com>
2020-05-09 14:10:15 +0800

    算法

--- leetcode/index.js
+++ leetcode/index.js
@@ -17,3 +17,49 @@ function addString(){
 }
 
 addString()
+
+
+/**
+ *  用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数*  和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ */
+
+
+ /**
+  * 输入：
+    ["CQueue","appendTail","deleteHead","deleteHead"]
+    [[],[3],[],[]]
+    输出：[null,null,3,-1]
+    示例 2：
+    输入：
+    ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+    [[],[],[5],[2],[],[]]
+    输出：[null,-1,null,null,5,2]
+  */
+ var CQueue = function() {
+    this.stack1 = []
+    this.stack2 = []
+  };
+  /**
+ * @param {number} value
+ * @return {void}
+ */
+
+ CQueue.prototype.appendTail = function(value) {
+    this.stack1.push(value);
+  };
+  
+  /**
+   * @return {number}
+   */
+  CQueue.prototype.deleteHead = function() {
+    if (this.stack2.length) {
+      return this.stack2.pop();
+    } else if (this.stack1.length) {
+      while(this.stack1.length) {
+        this.stack2.push(this.stack1.pop())
+      }
+      return this.stack2.pop();
+    } else {
+      return -1
+    }
+  };
