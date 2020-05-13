717e5104e28c63b1bc52ad7b944ef43451560b7f
hzxfjmc@163.com <huangzuxing@youxin.com>
2020-05-13 10:57:15 +0800

    二叉树中序遍历

--- leetcode/binaty_tree.js
+++ leetcode/binaty_tree.js
@@ -26,4 +26,49 @@ function sortBinaty_tree(arr){
     return root
 }
 
-sortBinaty_tree([-10-3-0,5,9])
+// sortBinaty_tree([-10-3-0,5,9])
+
+
+/**
+ * 二叉树中序遍历
+ * 输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+   输出: [1,3,2]
+ */
+// 递归版本
+var inOrderTraverse = (root) => {
+    let result = []
+    var inOrder  = (node) => {
+        if(node) {
+            // 先遍历左子树
+            inOrder (node.left)
+           // 再根节点
+            result.push(node.val)
+            // 最后遍历右子树
+            inOrder (node.right)
+        }
+    }
+    inOrder(root)
+    return result
+};
+
+//迭代版本
+var inOrderTraverse = function(root){
+    let list = [];
+    let stack = [];
+    let node = root
+    while(stack.length ||node){
+        if(node){
+            stack.push(node)
+            node = node.left;
+        }
+        node = stack.pop();
+        list.push(node.val)
+        node = node.right
+    }
+    return list
+}
