a6a42eb5f99f720e2980470c8d7047d53c757c6a
hzxfjmc@163.com <hzxfjmc@163.com>
2021-03-16 19:43:30 +0800

    ...

---
+++ js/index.js
@@ -0,0 +1,73 @@
+Function.prototype.mybind = function(context){
+    if(typeof this != 'Function'){
+        return new TypeError('error')
+    }
+    let this_ = this;
+    let args = [...arguments].slice(1)
+    return function func(){
+        if(this instanceof func){
+            return this_(...args,...arguments)
+        }else{
+            return this_.apply(context,args.concat(...arguments))
+        }
+    }
+}
+
+
+function deepCopy(souce){
+    if(!souce){
+        return souce
+    }
+    let obj = Array.isArray(souce)?[]:{}
+    for(let i in souce){
+        if(souce.hasOwnProperty(i)){
+            if(typeof souce[i] === 'object'){
+                 obj[i] = deepCopy(souce[i])
+            }else{
+                 obj[i] = souce[i]
+            } 
+        }
+    }
+    console.log(obj)
+    return obj
+}
+
+deepCopy({a:[1,2],v:3})
+
+
+Function.prototype.myCall=function(context){
+    if(typeof this != 'Function'){
+        return new TypeError('...')
+    }
+     context = context || window
+     context.fn = this;
+     let args = [...argument].slice(1)
+     let result = context.fn(...args)
+     delete context.fn
+     return result
+}
+
+Function.prototype.myapply=function(context) {
+    if(typeof this != 'Function') return new TypeError('...')
+    context = context || window
+    context.fn = this;
+    let result;
+    if(arguments[1]){
+       result = context.fn(...arguments[1])
+    }else{
+        result = context.fn()
+    }
+    delete context.fn;
+    return result
+}
+
+
+function new_(){
+    let obj = {}
+    let fn = [].shift.call(arguments)
+    obj.__proto__ = fn.prototype
+    let result = fn.apply(obj,arguments)
+    return typeof result === 'object' ? result : obj 
+}
+
+
---
+++ js/index.md
@@ -0,0 +1,40 @@
+1.js基本数据类型
+number、string、undefined、null、symbel、boolean  引用类型 object
+
+2.闭包是什么
+
+1、闭包是有权访问另一个函数作用域中的变量的函数
+
+2、闭包的使用：
+
+3.正则匹配
+
+var s = '12345237987584564656,000.00';
+var d=s.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+
+
+let s = ' 3434'
+s.replace(/(^\s*)|(\s*$)/g,'')
+
+
+4.移动端适配禁止屏幕缩放
+<meta name="viewport" content="width=device-width,initial-scale=1,maxinum-scale=1,user-scalble=no">
+
+5.http状态码
+
+301、308 URL永远重定向，目标请求URL应该更换新的请求链接
+302、307 URL临时性重定向，目标请求应该使用当前的请求方式
+304 协商缓存，通过与服务端协商返回上一次更做更改的内容
+
+
+6.缓存
+1.缓存主要是解决减少不必要的资源浪费，减少http请求来提升用户的体验。
+2.强缓存：
+    2.1强缓存主要包括expires、cache-control
+        2.1.1 expires http 1.0中定义的缓存机制，服务端在re response header中增加expires字段来表示资源的过时间戳 e1:expires: Thu,03 Jan 2019 11:43:03 
+        2.1.2 cache-control http 1.1 简单可以理解cache-control是expires的升级版 当cache-control与expires同时存在时优先cache-control e1：cache-control：public,max-age = 3600,s-maxage=3600
+3.协商缓存：
+    3.1协商缓存主要包含last-modified、etag
+        3.1.1 last-modified用来记录资源最后修改的时间，启用后请求资源在request请求头会加上last-modifed字段来
+        3.1.2 etag 优越性比last-modified更好能更加精准的感知资源是否有变化而去判断是否使用缓存。使用在请求头加上一段哈希值
+
